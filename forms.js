4c6989aa262f52f5831ccaec9b67ba6becca9387
Joe Mielzarek <joseph.mielzarek@gmail.com>
2016-05-01 20:16:55 -0500

    second commit, challenge complete

--- forms.js
+++ forms.js
@@ -1,24 +1,66 @@
 $(document).ready(function() {
-    var array = [];
-    $('#employeeinfo').on('submit', function(event) {
-      event.preventDefault();
+  var staffMembers = [];
 
-      var values = {};
-      $.each($('#employeeinfo').serializeArray(), function(i, field) {
-        values[field.name] = field.value;
-      })
+  $('#employeeinfo').on('submit', function(event) {
+    event.preventDefault();
 
-      console.log(values);
+    var values = {};
+    $.each($('#employeeinfo').serializeArray(), function(i, field) {
+      values[field.name] = field.value;
 
-      appendDom(values);
+    })
+
+    $('#employeeinfo').find('input[type=text]').val('');
+
+    //Pushes newly created employee objects from data inputs to array
+    staffMembers.push(values);
+
+    //Calls function that appends employee data inputs from user to DOM
+    appendDom(values);
+
+    //Calls functions that append and calculate salaries, respectively
+    appendSalary(calcSalaries(staffMembers));
+
+    //Removes entry when clicked
+    $('.person').on('click', 'button', function() {
+      $(this).parent().remove();
     });
 
-    function appendDom(empInfo) {
-      $('#container').append('<div class="person"></div>');
-      var $el = $('#container').children().last();
+  });
 
-      $el.append('<p>' + empInfo.employeefirstname + '</p>');
+  //Calculate salaries for each set of employee inputs
+  function calcSalaries(array) {
+    var totalSalary = 0;
+    var monthlySalary = 0;
+    for (var i = 0; i < array.length; i++) {
+      console.log(array[i]);
+      totalSalary += parseInt(array[i].employeesalary);
+      monthlySalary = Math.round(totalSalary / 12);
     }
+    return monthlySalary;
+  }
 
+  function appendSalary(monthlySalary) {
+
+    $('#container').append('<div class="payroll"></div>');
+    var $pr = $('#container').children().last();
+
+    $pr.append('<p>Total Salary Cost per Month: ' + monthlySalary + '</p>');
+  }
+
+  function appendDom(empInfo) {
+    $('#container').append('<div class="person"></div>');
+    var $el = $('#container').children().last();
+
+    $el.append('<p>First Name: ' + empInfo.employeefirstname + '</p>');
+    $el.append('<p>Last Name: ' + empInfo.employeelastname + '</p>');
+    $el.append('<p>Employee ID: ' + empInfo.employeeID + '</p>');
+    $el.append('<p>Job Title: ' + empInfo.employeejobtitle + '</p>');
+    $el.append('<p>Annual Salary: ' + empInfo.employeesalary + '</p>');
+
+    //Delete button
+    $el.append('<button>Remove Submission</button>');
+  }
 
 });
+
